0f7e9d68677b5135ecbbfd4b3e024c63cc97e0b1
Anastasia <63566470+Anastasiapixel@users.noreply.github.com>
2024-07-04 16:23:24 +0400

    feat: implement request to SWAPI

--- ReactRss/src/App.tsx
+++ ReactRss/src/App.tsx
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import { Forms } from "./components/forms";
 import { Result } from "./components/result";
+
+const getPeople = async (name) => {
+ const response = await fetch(
+  `https://swapi.dev/api/people/?search=${name}&format=json`
+ );
+ const data = await response.json();
+ return data;
+};
 export default class SearchApp extends Component {
+
+  // handleChangeName = (name) => {
+  //      this.setState({
+  //        name:
+  //      });
+  // }
   render() {
     return (
       <div className="block">
         <div className="first">
-          <Forms />
+          <Forms getPeople={getPeople} />
         </div>
         <div className="second">
-         <Result />
+          <Result />
         </div>
       </div>
     );
--- ReactRss/src/components/forms.tsx
+++ ReactRss/src/components/forms.tsx
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
 
 export class Forms extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+  }
+  onUpdateSearch = (e) => {
+    const name = e.target.value;
+    this.setState({ name });
+    // this.props.onUpdateSearch(name);
+  };
+  onUpdateStorage = async(e) => {
+    e.preventDefault();
+    localStorage.setItem("name", this.state.name);
+    const characters = await this.props.getPeople(this.state.name);
+    console.log(characters);
+    
+  };
+  
   render() {
     return (
-        <form className="search" action="">
-          <input type="text" name="" id="" />
-          <button>Search</button>
-        </form>
+      <form className="search" action="">
+        <input onChange={this.onUpdateSearch} type="text" name="" id="" />
+        <button onClick={this.onUpdateStorage}>Search</button>
+      </form>
     );
   }
 }
